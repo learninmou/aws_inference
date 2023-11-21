1f600d6a69946d2a46465260bf7a3b4dfc986958
root <root@ip-10-0-29-187.us-west-2.compute.internal>
2023-11-21 16:13:04 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,9 @@
+set -ex
+
 timedatectl set-timezone Asia/Shanghai
 
 apt update
 apt install -y python3-pip
-apt install -y awscli
+#apt install -y awscli
 #aws configure
-pip3 install boto3 uvicorn fastapi httpx sagemaker
+pip3 install boto3 uvicorn fastapi httpx sagemaker gunicorn
