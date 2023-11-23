ec071ad75d2d3b83425d397c39df50f27ac838d3
LI MOU <142368437+learninmou@users.noreply.github.com>
2023-11-23 10:20:02 +0800

    Create run_server.sh

---
+++ run_server.sh
@@ -0,0 +1,16 @@
+CURDIR=$(cd "$(dirname "$0")"; pwd)
+cd $CURDIR
+
+export ENV_LOG_DIR="${CURDIR}/logs"
+export ENV_AWS_ENDPOINTS_FILE="${CURDIR}/endpoints.ini"
+export ENV_AWS_AK=""
+export ENV_AWS_SK=""
+
+mkdir -p $ENV_LOG_DIR
+gunicorn launcher:app \
+    --bind 0.0.0.0:8080 \
+    --workers 3 \
+    --threads 1 \
+    --timeout 90 \
+    --worker-class "uvicorn.workers.UvicornWorker" \
+    --access-logfile "${ENV_LOG_DIR}/access.log"
