e1d1cfc59d149a09b567e5e92aa78e020138e5ad
LI MOU <142368437+learninmou@users.noreply.github.com>
2023-12-06 16:33:45 +0800

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -9,7 +9,7 @@ export ENV_AWS_SK=""
 mkdir -p $ENV_LOG_DIR
 gunicorn launcher:app \
     --bind 0.0.0.0:8080 \
-    --workers 3 \
+    --workers 8 \
     --threads 1 \
     --timeout 90 \
     --worker-class "uvicorn.workers.UvicornWorker" \
